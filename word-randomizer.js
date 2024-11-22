882a136728319f31c08ec39af7fe2a89f6753d88
paria <pariyataba@gmail.com>
2024-11-22 09:17:05 +0100

    add js to section 1 and 2

--- word-randomizer.js
+++ word-randomizer.js
@@ -2,7 +2,7 @@ import { words } from "./svenska-ord.js";
 
 //input two numbers which you want the wordlegth to be. Returns a word within the specified range.
 //t.ex console.log(randomizeWord(17, 22));
-function randomizeWord(minLetters, maxLetters) {
+export function randomizeWord(minLetters, maxLetters) {
     let i = Math.floor(Math.random() * words.length);
     let word = words[i];
     do {
