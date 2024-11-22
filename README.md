e97374c49c9083e2dc36dc792c30c9de626232ee
FBetulDemir <fatmabetuld@gmail.com>
2024-11-22 11:48:25 +0100

    Added function to save scores

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Diamond-Experts
+
 Grupparbete Javascript FED24
+
+first day logbook (21/11/2024) - Betül
+
+-   worked with creating a function to handle user guesses,
+-   added an event listener to the keyboard,
+
+Second day logbook (22/11/2024) - Betül
+
+    - continiued working with the function to handle user guesses,
+    - created conditions to check win/lose situations,
+    - added gameOver to disable game when win/lose
--- score.js
+++ score.js
@@ -1,7 +1,9 @@
-// import { words } from './svenska-ord';
+// import { words } from './svenska-ord'
+
 const displaySection = document.querySelector('.mystery-word');
 const userInput = document.querySelector('.user-input');
 const resultScreen = document.querySelector('.result-screen');
+const highestScores = document.querySelector('.highest-scores');
 
 let testWord = 'frontend';
 let guessedLetters = [];
@@ -10,7 +12,7 @@ let maxAttempts = 6;
 let gameBoard = Array(testWord.length).fill('_');
 let score = 0;
 let gameOver = false;
-
+let userName = 'Player 1';
 const KEY = 'scores';
 
 function displayInitialBoard() {
@@ -42,7 +44,7 @@ document.addEventListener('keydown', (event) => {
     console.log(guessedLetters);
 });
 
-const handleGuess = (pressedLetter, word) => {
+export const handleGuess = (pressedLetter, word) => {
     pressedLetter = pressedLetter.toLowerCase();
     if (
         guessedLetters.includes(pressedLetter) ||
@@ -81,22 +83,69 @@ const handleGuess = (pressedLetter, word) => {
     //Here is the code for deciding winning or losing condition.
     if (!gameBoard.includes('_')) {
         resultScreen.innerHTML = 'You win!';
+        score++;
         gameOver = true;
+        saveScores({
+            word: testWord,
+            user: userName,
+            score: score,
+            date: new Date().toString(),
+        });
+
+        console.log('High Scores:', highScores());
         // We can add do you want to play again and play again button here
     } else if (wrongGuesses.length >= maxAttempts) {
         resultScreen.innerHTML = `Game Over! The word was "${word}".`;
         gameOver = true;
+        saveScores({
+            word: testWord,
+            user: userName,
+            score: score,
+            date: new Date().toString(),
+        });
+
         // We can add do you want to play again and play again button here
     }
+
+    displayInitialBoard();
 };
 
 handleGuess();
 
-// function highScores() {
-//     let json = localStorage.getItem(KEY);
-//     let scores = JSON.parse(json);
-//     console.log('Highest scores', scores);
-//     return scores;
-// }
+export function resetGame() {
+    guessedLetters = [];
+    wrongGuesses = [];
+    maxAttempts = 6;
+    gameBoard = Array(testWord.length).fill('_');
+    gameOver = false;
+    displayInitialBoard();
+    userInput.innerHTML = '';
+    resultScreen.innerHTML = '';
+    console.log('Game reset');
+}
 
-// function saveScores(scores) {}
+resetGame();
+
+// saving scores to the local storage starts here
+
+export function saveScores(newScore) {
+    let getScores = highScores();
+    getScores.push(newScore);
+
+    // Sort  score -highest to lowest within the top 5 scores
+    getScores.sort((a, b) => b.score - a.score);
+
+    // Save top 5 scores will be displayed
+    // We can show more or less scores
+    getScores = getScores.slice(0, 5);
+
+    localStorage.setItem(KEY, JSON.stringify(getScores));
+    console.log('Scores saved:', getScores);
+}
+
+export function highScores() {
+    const json = localStorage.getItem(KEY);
+    return json ? JSON.parse(json) : [];
+}
+
+// display function
